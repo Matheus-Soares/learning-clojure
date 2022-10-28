edf4db877d3c025055af429127a74c99c48d6b9c
Matheus Soares <matheus.asoares2@gmail.com>
2022-10-28 01:08:10 -0300

    Starting to learn about collections

--- hello-world/src/hello_world/vectors.clj
+++ hello-world/src/hello_world/vectors.clj
@@ -25,3 +25,27 @@
   (+ value 10))
 
 (println "Update pos 0 with custom function increment-10:" (update prices 0 increment-10))
+
+; Map aplica uma função em um conjunto de valores
+(println "Increment 10:" (map increment-10 prices))
+
+(println "Range from 0 to 9: (range 10)")
+
+; filter função vetor
+(println "Just odd numbers:" (filter odd? (range 10)))
+
+(defn is-more-than-500?
+  [value]
+  (> value 500))
+
+(println "More than 500: " (filter is-more-than-500? prices))
+
+; Reduce função [valor inicial] vetor
+(println "Sum of all prices:" (reduce + prices))
+
+(defn sum-ten-percent-of-value
+  [value-1, value-2]
+  (+ value-1 (* value-2 0.1)))
+
+; Função deve receber dois valores, o primeiro é a soma e o segundo é o próximo valor
+(println "Reduce with custom function: " (reduce sum-ten-percent-of-value 0 [100 100]))
